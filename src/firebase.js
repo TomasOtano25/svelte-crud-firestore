b09b68a29083284bf0259fa931ea3892fd4e18b0
tomasotano25 <tomasotano25@gmail.com>
2022-03-19 19:43:47 -0400

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {  } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD7PQD5xkpCI47mIeEG4KDkqZAoJBWaYk",
+  authDomain: "svelte-crud-c814d.firebaseapp.com",
+  projectId: "svelte-crud-c814d",
+  storageBucket: "svelte-crud-c814d.appspot.com",
+  messagingSenderId: "913144735223",
+  appId: "1:913144735223:web:1e1d5234da546018f1baf2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
